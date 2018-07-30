663b7a624f84bd11030ac3f5889469cd82d424ff
Eugene Gulevich <gulevich.evg@gmail.com>
2018-07-30 08:47:32 +0300

    Add Point class

--- resources/src/GameObject/AbstractObject.ts
+++ resources/src/GameObject/AbstractObject.ts
@@ -1,28 +1,8 @@
 /// <reference path="../lib.d.ts/pixi.d.ts" />
 /// <reference path="../Utils/easy-html.ts" />
+/// <reference path="./Point.ts" />
 
 module GameObject {
-
-	export class Point {
-
-		constructor(
-			public _x: number = 0,
-			public _y: number = 0,
-			public update: Function
-		) {}
-
-		get x(): number { return this._x; }
-		set x(value: number) { this._x = value; this.update(this._x, this._y); }
-
-		get y(): number { return this._y; }
-		set y(value: number) { this._y = value; this.update(this._x, this._y); }
-
-		public set(x: number, y: number): void {
-			this._x = x;
-			this._y = y;
-			this.update(this._x, this._y);
-		};
-	}
 
 	interface IAbstractInitParameters {
 		name?: string;
---
+++ resources/src/GameObject/Point.ts
@@ -0,0 +1,22 @@
+module GameObject {
+	export class Point {
+
+		constructor(
+			public _x: number = 0,
+			public _y: number = 0,
+			public update: Function
+		) {}
+
+		get x(): number { return this._x; }
+		set x(value: number) { this._x = value; this.update(this._x, this._y); }
+
+		get y(): number { return this._y; }
+		set y(value: number) { this._y = value; this.update(this._x, this._y); }
+
+		public set(x: number, y: number): void {
+			this._x = x;
+			this._y = y;
+			this.update(this._x, this._y);
+		};
+	}
+}
